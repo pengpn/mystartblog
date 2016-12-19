747d5a4acd5e88b35b67fc06dbc8c2945fa53350
pengpn <312924860@qq.com>
2016-12-19 11:25:53 +0800

    add change password module

--- application/controllers/admin/Others.php
+++ application/controllers/admin/Others.php
@@ -10,6 +10,7 @@ class Others extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->model('Siteinfo_model');
+
     }
 
     public function show_siteinfo(){
@@ -32,5 +33,46 @@ class Others extends CI_Controller{
         $this->load->view('admin/menu', $data);
         $this->load->view('admin/others_setsiteinfo_success', $data);
         $this->load->view('admin/footer');
+    }
+
+    public function change_password(){
+        $this->load->library('form_validation');
+        $username = $this->session->userdata('username');
+        $this->db->where('username',$username);
+        $this->user_info = $this->db->get('user')->result_array();
+
+        $this->form_validation->set_rules('old_password','old_password','trim|callback_password_check');
+        $this->form_validation->set_rules('new_password','new_password','trim');
+        $this->form_validation->set_rules('new_password_conf','new_password_conf','trim|matches[new_password]');
+        $this->form_validation->set_message('matches','两次输入不一致');
+        $this->form_validation->set_error_delimiters('<p class="text-danger">','</p>');
+
+        $data['cur_title'] = array('','','','','','','');
+        if($this->form_validation->run() == false){
+            $this->load->view('admin/header');
+            $this->load->view('admin/menu',$data);
+            $this->load->view('admin/others_change_password');
+            $this->load->view('admin/footer');
+        }else{
+            $new_password = array(
+                'password' => password_hash($this->input->post('new_password',true),PASSWORD_DEFAULT),
+            );
+            $this->db->where('username',$username);
+            $this->db->update('user',$new_password);
+
+            $this->load->view('admin/header');
+            $this->load->view('admin/menu', $data);
+            $this->load->view('admin/others_change_password_success');
+            $this->load->view('admin/footer');
+        }
+    }
+
+    public function password_check($str){
+        $password = isset($this->user_info[0]['password'])?$this->user_info[0]['password']:0;
+        if(!password_verify($str,$password)){
+            $this->form_validation->set_message('password_check','原密码错误');
+            return false;
+        }
+        return true;
     }
 }
