ef98603078e1eef03c4deb1bad0404a2a9ef98ab
pengpn <312924860@qq.com>
2016-12-29 15:17:34 +0800

    update footer info

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,14 +3,14 @@
         <div class="am-u-sm-12 am-u-md-4- am-u-lg-4">
             <h3>关于 StartBlog</h3>
             <p class="am-text-sm">一款基于Codeigniter、Amazeui(Html5)开发的简洁、易用、跨平台自适应的Markdown博客系统.<br> 使用Amaze UI 跨屏前端框架实现模板 <br> 支持多平台、终端<br>嗯嗯嗯，不知道说啥了。感谢这些开源的程序<br><br>
-            Made with in Startblog v2.1 <a href="https://github.com/Cryin/Startblog"><span class="am-icon-github"></span> </a></p>
+            Made with in Startblog v2.1 <a href="https://github.com/pengpn/mystartblog"><span class="am-icon-github"></span> </a></p>
         </div>
         <div class="am-u-sm-12 am-u-md-4- am-u-lg-4">
             <h3>社交账号</h3>
             <p>
-                <a href="tencent://message/?uin=416049355"><span class="am-icon-qq am-icon-fw am-primary blog-icon blog-icon"></span></a>
-                <a href="https://github.com/Cryin/"><span class="am-icon-github am-icon-fw blog-icon blog-icon"></span></a>
-                <a href="http://weibo.com/justear"><span class="am-icon-weibo am-icon-fw blog-icon blog-icon"></span></a>
+                <a href="tencent://message/?uin=312924860"><span class="am-icon-qq am-icon-fw am-primary blog-icon blog-icon"></span></a>
+                <a href="https://github.com/pengpn/"><span class="am-icon-github am-icon-fw blog-icon blog-icon"></span></a>
+                <a href="http://weibo.com/pnnn"><span class="am-icon-weibo am-icon-fw blog-icon blog-icon"></span></a>
                 <a href=""><span class="am-icon-weixin am-icon-fw blog-icon blog-icon"></span></a>
                 <a href="<?php echo base_url('/feed')?>"><span class="am-icon-rss am-icon-fw blog-icon blog-icon"></span></a>
             </p>
