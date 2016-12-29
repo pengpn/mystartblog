d8e75a62b6c1e69676052f3853e63060c4f237f0
pengpn <312924860@qq.com>
2016-12-29 14:58:29 +0800

    update info

--- application/views/about.php
+++ application/views/about.php
@@ -69,10 +69,10 @@ var duoshuoQuery = {short_name:"startblog"};
         <div class="blog-sidebar-widget blog-bor">
             <h2 class="blog-text-center blog-title"><span>Contact ME</span></h2>
             <p>
-                <a href="tencent://message/?uin=416049355"><span class="am-icon-qq am-icon-fw am-primary blog-icon"></span></a>
-                <a href="https://github.com/Cryin/"><span class="am-icon-github am-icon-fw blog-icon"></span></a>
-                <a href="http://weibo.com/justear"><span class="am-icon-weibo am-icon-fw blog-icon"></span></a>
-                <a href="http://www.startblog.cc/"><span class="am-icon-weixin am-icon-fw blog-icon"></span></a>
+                <a href="tencent://message/?uin=312924860"><span class="am-icon-qq am-icon-fw am-primary blog-icon"></span></a>
+                <a href="https://github.com/pengpn/"><span class="am-icon-github am-icon-fw blog-icon"></span></a>
+                <a href="http://weibo.com/pnnn"><span class="am-icon-weibo am-icon-fw blog-icon"></span></a>
+                <a href="http://www.xxxx.cc/"><span class="am-icon-weixin am-icon-fw blog-icon"></span></a>
                 <a href="<?php echo base_url('/feed')?>"><span class="am-icon-rss am-icon-fw blog-icon"></span></a>
             </p>
         </div>
